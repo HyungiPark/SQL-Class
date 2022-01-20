329d969e6086d788a4ce1658a1fe6bce43b5cfc5
HyungiPark <97497698+HyungiPark@users.noreply.github.com>
2022-01-20 17:19:13 +0900

    Update selfJoin.sql

--- selfJoin.sql
+++ selfJoin.sql
@@ -8,8 +8,8 @@ select employee_id, first_name || ' ' ||  last_name "FULLNAME" , manager_id from
 select employee_id, first_name || ' ' ||  last_name "FULLNAME" , manager_id from employees
 where employee_id=123;
 
-select e.employee_id, e.first_name || ' '|| e.last_name "Employee Name",d.department_id, d.department_name,l.location_id,l.state_province,
-l.city
+select e.employee_id, e.first_name || ' '|| e.last_name "Employee Name",
+d.department_id, d.department_name,l.location_id,l.state_province,l.city
 from employees e ,departments d , locations l where e.department_id =d.department_id 
 and d.location_id = l.location_id
 --and e.employee_id=164
